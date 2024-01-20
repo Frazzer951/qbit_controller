8ac9124cc263643e8049013e5c3adf23f35061cd
Luke <luke343279@gmail.com>
2024-01-20 13:26:56 -0800

    qbit setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use qbit_rs::model::Credential;
+use qbit_rs::Qbit;
+
+#[tokio::main]
+async fn main() {
+    let credential = Credential::new("admin", "adminadmin");
+    let api = Qbit::new("http://10.10.20.20:8282/", credential);
+    let torrents = api.get_version().await;
+
+    match torrents {
+        Ok(torrents) => {
+            println!("{torrents}");
+        }
+        Err(e) => println!("Error: {}", e),
+    }
 }
